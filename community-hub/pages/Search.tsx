087c3dc11f000417fbff63243771cbaf6fe190a0
hemantmm <hemant29mehta@gmail.com>
2023-06-18 20:36:26 +0530

    Enable

--- community-hub/pages/Search.tsx
+++ community-hub/pages/Search.tsx
@@ -1,5 +1,12 @@
+import Link from "next/link";
+
 export default function Search() {
     return(
-        <h2>hemant</h2>
+        // <h2>hemant</h2>
+        <div>
+            <div>
+                <Link href="/"><h2 className="border">NFT Connect</h2></Link>
+            </div>
+        </div>
     )
 }
--- community-hub/pages/index.tsx
+++ community-hub/pages/index.tsx
@@ -1,45 +1,10 @@
-// import Image from 'next/image'
-// import { Inter } from 'next/font/google'
-// import page from './page'
-
-import { useRouter } from "next/router";
-import Page from "./Page";
-import { useState } from 'react';
-import Link from "next/link";
 import Homes from "./Home";
-// import { HuddleIframe } from "@huddle01/iframe/dist/declarations/src";
 
-// const inter = Inter({ subsets: ['latin'] })
-
-export default function Home() {
-
-  const router=useRouter()
-
-  const [showHuddle,setShowHuddle]=useState(false)
-
-  const startMeet=()=>{
-    // router.push(<Page />)
-    setShowHuddle(true)
-    router.push('/Page')
-  }
-
-  const Change=()=>{
-
-  }
-
-  
+export default function Home() {  
 
   return (
     <div>
-    {/* <h2>hemant</h2> */}
-    {/* <button className=" text-2xl" onClick={startMeet}>click</button> */}
-    
-    {/* {showHuddle && <Page />} */}
-    {/* <button>Home</button> */}
     <Homes />
-    {/* <Home /> */}
-    {/* {showHuddle && <Page />} */}
-
     </div>
   )
 }
